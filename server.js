e2ef3474d9e896048e638c78da6a36fc1949920f
coderonfleek <fikfikky@gmail.com>
2021-09-02 20:07:18 +0100

    HMAC Validation added

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const routes = require("./routes");
 const bodyParser = require('body-parser');
 const { startDatabase } = require("./database");
+const crypto = require('crypto');
 
 // App
 const app = express();
 
+const sigHeaderName = 'X-Hub-Signature-256';
+const sigHashAlg = 'sha256';
+const secret = "ABCD1234";
 
-app.use(bodyParser.json());
+
+app.use(bodyParser.json(
+    {
+        verify: (req, res, buf, encoding) => {
+            if (buf && buf.length) {
+            req.rawBody = buf.toString(encoding || 'utf8');
+            }
+        },
+    }
+));
 
 //HMAC Middleware
 /* var signature = req.get('X-PDF-Signature', 'sha1=')
@@ -40,6 +53,35 @@ const dbSetup = async (req, res, next) => {
 
 app.use(dbSetup);
 
+//Validate payload
+function validatePayload(req, res, next) {
+    /* let signature = req.get('x-hub-signature', 'sha1=');
+
+    let bodyCrypted = crypto.createHmac('sha1', '1234ABCD')
+    .update(JSON.stringify(req.body)).digest('hex')
+
+    if (bodyCrypted !== signature) {
+        res.status(401).send();
+        return
+    }else{
+        next();
+    } */
+    if (!req.rawBody) {
+        return next('Request body empty')
+    }
+
+    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+    const hmac = crypto.createHmac(sigHashAlg, secret)
+    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+
+    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+        return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+    }
+
+    return next()
+
+}
+app.use(validatePayload);
 app.use('/', routes);
 
 // Server
