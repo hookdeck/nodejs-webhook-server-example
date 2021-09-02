4568540aff0c809f13ac747bbfe98f1cd699051f
coderonfleek <fikfikky@gmail.com>
2021-09-02 20:16:37 +0100

    Fixed HMAC issues

--- server.js
+++ server.js
@@ -22,20 +22,6 @@ app.use(bodyParser.json(
     }
 ));
 
-//HMAC Middleware
-/* var signature = req.get('X-PDF-Signature', 'sha1=')
-
-var bodyCrypted = require('crypto')
-.createHmac('sha1', '12345')
-.update(JSON.stringify(req.body))
-.digest('hex')
-
-if (bodyCrypted !== signature) {
-res.status(401).send()
-return
-}
-
-console.log('PDF webhook received', JSON.stringify(req.body)) */
 
 // Set port
 const port = process.env.PORT || "1337";
@@ -54,34 +40,26 @@ const dbSetup = async (req, res, next) => {
 app.use(dbSetup);
 
 //Validate payload
-function validatePayload(req, res, next) {
-    /* let signature = req.get('x-hub-signature', 'sha1=');
+/* function validatePayload(req, res, next) {
 
-    let bodyCrypted = crypto.createHmac('sha1', '1234ABCD')
-    .update(JSON.stringify(req.body)).digest('hex')
-
-    if (bodyCrypted !== signature) {
-        res.status(401).send();
-        return
-    }else{
-        next();
-    } */
-    if (!req.rawBody) {
-        return next('Request body empty')
-    }
-
-    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
-    const hmac = crypto.createHmac(sigHashAlg, secret)
-    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
-
-    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
-        return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+    if(req.method == "POST"){
+        if (!req.rawBody) {
+            return next('Request body empty')
+        }
+    
+        const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+        const hmac = crypto.createHmac(sigHashAlg, secret)
+        const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+    
+        if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+            return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+        }
     }
 
     return next()
 
 }
-app.use(validatePayload);
+app.use(validatePayload); */
 app.use('/', routes);
 
 // Server
