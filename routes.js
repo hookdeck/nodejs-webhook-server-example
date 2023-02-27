38410d35aec3a24e8e04d79c19ee67c51fe02e70
Rodriq <fogangrodriq@gmail.com>
2023-02-27 17:59:00 +0100

    added Checkout, Docker, BigCommerce, WooCommerce and fix routes structure

--- routes.js
+++ routes.js
@@ -9,44 +9,72 @@ router.get("/", function(req, res) {
   res.send("Welcome to the Webhooks API");
 });
 
-router.post("/okta-webhooks-endpoint", function(req, res) {
-  console.log(req.body);
-  res.send("Okta Event hook Successfully received");
-});
+// PAYMENTS
 
 router.post("/stripe-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
   console.log(req.body);
   res.send("Stripe Successfully received Webhook request");
 });
 
-router.post("/paypal-webhooks-endpoint", bodyParser.raw({ type: 'application/json' }), function(req, res) {
+router.post("/paypal-webhooks-endpoint", bodyParser.raw({ type: 'application/json'}), function(req, res) {
 	console.log(req.body);
 	res.send("Paypal Successfully received Webhook request");
 });
 
-router.post("/paddle-webhooks-endpoint", bodyParser.raw({ type: 'application/x-www-form-urlencoded' }), function(req, res) {
+router.post("/paddle-webhooks-endpoint", bodyParser.raw({ type: 'application/x-www-form-urlencoded'}), function(req, res) {
 	console.log(qs.parse(decodeURIComponent(req.body)));
 	res.send("Paddle Successfully received Webhook request");
 });
 
-router.post("/github-webhooks-endpoint", function(req, res) {
+router.post("/checkout-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
+  console.log(req.body);
+  res.send("Checkout Successfully received Webhook request");
+});
+
+// CI/CD
+
+router.post("/github-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
   console.log(req.body);
   res.send("GitHub Successfully received Webhook request");
 });
 
-router.post("/gitlab-webhooks-endpoint", function(req, res) {
+router.post("/gitlab-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
   console.log(req.body);
   res.send("Gitlab Successfully received Webhook request");
 });
 
-router.post("/bitbucket-webhooks-endpoint", function(req, res) {
+router.post("/bitbucket-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
   console.log(req.body);
   res.send("Bitbucket Successfully received Webhook request");
 });
 
-router.post("/shopify-webhooks-endpoint", function(req, res) {
+router.post("/docker-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), bodyParser.raw({type: 'application/json'}), function(req, res) {
+  console.log(req.body);
+  res.send("Docker Successfully received Webhook request");
+});
+
+// E-COMM
+
+router.post("/shopify-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
   console.log(req.body);
   res.send("Shopify Successfully received Webhook request");
+});
+
+router.post("/bigcommerce-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
+  console.log(req.body);
+  res.send("BigCommerce Successfully received Webhook request");
+});
+
+router.post("/woocommerce-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
+  console.log(req.body);
+  res.send("WooCommerce Successfully received Webhook request");
+});
+
+// EXTRAS
+
+router.post("/okta-webhooks-endpoint", bodyParser.raw({type: 'application/json'}), function(req, res) {
+  console.log(req.body);
+  res.send("Okta Event hook Successfully received");
 });
 
 router.get("/todos", function(req, res) {
